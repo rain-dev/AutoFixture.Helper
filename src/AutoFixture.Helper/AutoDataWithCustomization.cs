4e1516c989b9b2482fce598049e051b683bfe7d0
Rainier Bacareza <81784594+rain-dev@users.noreply.github.com>
2024-02-17 18:20:43 +0800

    add starter projects (#1)
    - add build props
    - add Moq and NSubstitute projects
    - basic structures

---
+++ src/AutoFixture.Helper/AutoDataWithCustomization.cs
@@ -0,0 +1,32 @@
+﻿using AutoFixture.Helper;
+
+namespace AutoFixture.Moq.Helper;
+
+/// <summary>
+/// Custom attribute for providing auto-generated data specimens with customizations using AutoFixture.
+/// </summary>
+[AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
+public class AutoDataWithCustomization : AutoDataExtendedAttribute
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="AutoDataWithCustomization"/> class.
+    /// </summary>
+    /// <param name="customizations">An array of customization types to apply.</param>
+    public AutoDataWithCustomization(
+        params Type[] customizations)
+        :base(() =>
+        {
+            var fixture = new Fixture();
+            var customizationBuilder = new CustomizationBuilder(customizations);
+
+            var compositeCustomization = customizationBuilder.Build();
+
+            fixture.Customize(compositeCustomization);
+
+            return fixture;
+
+        })
+    {
+        
+    }
+}
---
+++ src/AutoFixture.Helper/OptionCustomization.cs
@@ -0,0 +1,24 @@
+﻿namespace AutoFixture.Helper;
+
+/// <summary>
+/// Option customization
+/// </summary>
+/// <typeparam name="TOption"></typeparam>
+public class OptionCustomization<TOption> : ICustomization
+    where TOption : class, new()
+{
+
+    /// <summary>
+    /// Adds customization for .NET Option
+    /// </summary>
+    /// <param name="fixture"></param>
+    public void Customize(IFixture fixture)
+    {
+        fixture.Customize<TOption>(f => f.FromFactory(() =>
+        {
+            var t = fixture.Build<TOption>().WithAutoProperties().Create();
+            return fixture.Build<TOption>().WithAutoProperties().Create();
+        }));
+
+    }
+}
